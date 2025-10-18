ca51a80309c10882efa8e1de1e0359d6813e02ca
WhoisJulie007 <maydeli.castan@iest.edu.mx>
2025-10-17 18:58:18 -0600

    terminando la clase del 17, hicimos muchas cosas perdi la cuenta

---
+++ Modelos y vistas dinamicas/ContentView5.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView5.swift
+//  Modelos y vistas dinamicas
+//
+//  Created by win603 on 17/10/25.
+//
+
+import SwiftUI
+
+struct ContentView5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView5()
+}
